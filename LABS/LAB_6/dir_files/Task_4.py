ca949e94dc4e1f41dc5e34cce3d907f2e01f9292
App1epli <alistynyshtyk07@gmail.com>
2025-02-25 11:24:15 +0500

    ⠀⠀⠀⠀⠀⠀⠀⠀⠀

--- LABS/LAB_6/dir_files/Task_4.py
+++ LABS/LAB_6/dir_files/Task_4.py
@@ -1,5 +1,5 @@
 #Write a Python program to count the number of lines in a text file.
-file = open('alis.txt', 'r')
+file = open('C:/Users/Admin/Documents/PP_2_labs/LABS/LAB_6/dir_files/alis.txt', 'r')
 
 cnt = 0
 for _ in file:
